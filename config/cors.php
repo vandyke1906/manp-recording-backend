eef345b3ec59ffc665caadeab79c828ace4bc7cc
vandyke1906 <vandyke1906@gmail.com>
2025-06-19 14:35:49 +0800

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
-    'allowed_origins' => ['https://mountapo-app.netlify.app', 'http://localhost:5173'], // Allow React frontend
+    'allowed_origins' => ['http://mountapo-app.netlify.app', 'https://mountapo-app.netlify.app', 'http://localhost:5173'], // Allow React frontend
     'allowed_origins_patterns' => [],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, //default is false
+    'supports_credentials' => true, //default is false
 ];
