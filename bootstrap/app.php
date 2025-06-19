e34062b1b957f00e1bac2b0ea557b8f9ff65d960
vandyke1906 <vandyke1906@gmail.com>
2025-06-19 10:42:52 +0800

    added proxy config: If you're deploying on Render (which uses reverse proxies), Laravel might not detect the correct scheme (https) unless you configure it to trust those proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use App\Http\Middleware\CorsMiddleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
-
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -30,6 +30,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
         $middleware->prepend(EnsureFrontendRequestsAreStateful::class);
         $middleware->prepend(CorsMiddleware::class); // ✅ Ensure it is globally applied
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO
+        );
+
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
