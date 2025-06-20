b886bc2379ccc3d07e99b4bd99f3dbd5d174b1f2
vandyke1906 <vandyke1906@gmail.com>
2025-06-20 15:29:31 +0800

    allow all origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
-    'allowed_origins' => ['http://mountapo-app.netlify.app', 'https://mountapo-app.netlify.app', 'http://localhost:5173'], // Allow React frontend
+    // 'allowed_origins' => ['http://mountapo-app.netlify.app', 'https://mountapo-app.netlify.app', 'http://localhost:5173'], // Allow React frontend
+    'allowed_origins' => ["*"], // Allow React frontend
     'allowed_origins_patterns' => [],
     'exposed_headers' => [],
     'max_age' => 0,
