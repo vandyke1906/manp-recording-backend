a44c7537431b63fd35c9a9b0e0923957950e3d77
vandyke1906 <vandyke1906@gmail.com>
2025-06-20 14:52:23 +0800

    updated the cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
     //     ]);
     // })
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(HandleCors::class); // ✅ Ensure it is globally applied
+        $middleware->prepend(CorsMiddleware::class); // ✅ Ensure it is globally applied
+        // $middleware->prepend(HandleCors::class); // ✅ Ensure it is globally applied
         // $middleware->statefulApi();
         $middleware->trustProxies(
             at: '*',
