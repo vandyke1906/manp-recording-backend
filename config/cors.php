31aae35a73076824640bee5934811e90a2847943
vandyke1906 <vandyke1906@gmail.com>
2025-07-18 22:21:02 +0800

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => ['https://mountapo-app.netlify.app','http://localhost:5173'], // ✅ NO "*"
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => ['Authorization', 'Content-Type', 'Content-Disposition'],
+    'exposed_headers' => ['Authorization', 'Content-Type'],
     'max_age' => 0,
     'supports_credentials' => false, // set to true only if cookies/auth
 ];
