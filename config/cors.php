05733ddf753f58c18d7be3f18a865ea033a6468d
vandyke1906 <vandyke1906@gmail.com>
2025-07-18 22:54:17 +0800

    update again

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://mountapo-app.netlify.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
